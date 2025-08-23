1eb06f4112eeb225d9138da21169dd80e8daf23b
Kacperbartlomiejczak <bartlomiejczakkapi@gmail.com>
2025-08-23 19:45:50 +0200

    Initial Commit

---
+++ WineUni-final/WineUni_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineUni_finalApp.swift
+//  WineUni-final
+//
+//  Created by Kacper Bartlomiejczak on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct WineUni_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
